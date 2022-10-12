e0f8c3b1158a822dad42b8c71572243fee13d093
Maxime Leroy <19607336+maxime1907@users.noreply.github.com>
2022-10-12 14:17:13 +0200

    feat: build static output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+BUILD_ROOT=`cat sourceknight.yaml | yq '.project.root' | tr -d '"'`
+BUILD_OUTPUT=`cat sourceknight.yaml | yq '.project.output' | tr -d '"'`
+if [[ "$BUILD_OUTPUT" == "null" || "$BUILD_OUTPUT" == "" ]]; then
+    BUILD_OUTPUT="${BUILD_ROOT}/addons/sourcemod/plugins"
+fi
+
+if [[ ${BUILD_OUTPUT::1} == "/" ]]; then
+    BUILD_OUTPUT=".${BUILD_OUTPUT}"
+fi
+
+echo "Build output: $BUILD_OUTPUT"
+
 sh -c "sourceknight $*"
+
+mkdir -p .sourceknight/package/common/addons/sourcemod/plugins
+
+if [[ -d "${BUILD_OUTPUT}" ]]; then
+    cp -R ${BUILD_OUTPUT}/* .sourceknight/package/common/addons/sourcemod/plugins
+fi
